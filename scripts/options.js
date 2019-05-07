870da35ce46561b8a0cc4cfcc9a20b7386bf5670
Eli Bittleston <dbittles@andrew.cmu.edu>
2019-05-07 15:46:12 -0400

    Added feature to change uas, added popup, reorganized file structure, bug fixes

--- scripts/options.js
+++ scripts/options.js
@@ -2,9 +2,25 @@
 
 var settings = {};
 var domainList = [];
+var setUAS = {};
 var UASoptions = ["iOS","Chrome","Safari","Firefox"];
 var unsaved = false;
 
+const UASlist = {
+    
+      win: {
+        Firefox: "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        Chrome: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+        },
+      mac: {
+        Firefox: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) Gecko/20100101 Firefox/42.0",
+        Chrome: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+        },
+      linux: {
+        Firefox: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
+        Chrome: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36",
+        }
+    };
 
 //Check if settings data exists, initialize it
 function initPage(){
@@ -28,13 +44,50 @@ function initPage(){
         console.log("No domainList data found");
     }
   });
+  chrome.storage.sync.get('setUAS', function(result) {
+    // check if data exists.
+    if (result) {
+        setUAS = result['setUAS'];
+        console.log("Found setUAS data");
+        var ddBrowser = document.getElementById("browserDropdown");
+        var ddOS = document.getElementById("osDropdown");
+        //set browser dropdown
+        var browserOpts = ddBrowser.options.length;
+        for (var i=0; i<browserOpts; i++){
+            if (ddBrowser.options[i].value == setUAS['browser']){
+                ddBrowser.options[i].selected = true;
+                break;
+            }
+        }
+        //set OS dropdown
+        var osOpts = ddOS.options.length;
+        for (var i=0; i<osOpts; i++){
+            if (ddOS.options[i].value == setUAS['os']){
+                ddOS.options[i].selected = true;
+                break;
+            }
+        }
+        document.getElementById('newUAS').textContent = UASlist[setUAS['os']][setUAS['browser']]; 
+    } else {
+        console.log("No settings data found");
+    }
+  });
   var d = new Date();
   document.getElementById('curyear').innerHTML = d.getFullYear();
-  
-  
+  document.getElementById('curUAS').textContent = navigator.userAgent; 
+
+  document.getElementById('browserDropdown').addEventListener("change", updateNewUAS);
+  document.getElementById('osDropdown').addEventListener("change", updateNewUAS);
+
+
 }
 
-
+function updateNewUAS(){
+  var ddBrowser = document.getElementById("browserDropdown");
+  var ddOS = document.getElementById("osDropdown");
+  document.getElementById('newUAS').textContent = UASlist[ddOS.value][ddBrowser.value];
+  unsaved = true;
+}
 
 
 
@@ -49,27 +102,29 @@ function buildHtmlTable(selector) {
     var newRow = {};
     // Add delete column
     newRow[""] = "<div class='delRow'><i class='fas fa-minus-circle'></i> <span class='delTag'>&nbspDelete</span></div>";
+    // Add active column
+    newRow["Active"] = '<label class="switch"> <input type="checkbox"'+(settings[domain][0] ? "checked" : "")+'> <span class="slider round"></span> </label>';
     //Add domain column
-    newRow["Domain"] = "<input name='domainInput' class='domainInput' type='text' placeholder='google.com' value='"+domain+"'></input>";
+    newRow["Domain"] = "<input name='domainInput' class='domainInput' type='text' placeholder='example.com' value='"+domain+"'></input>";
     //Add UAS column
-    var optionsAsString = "";
-    for(var i = 0; i < UASoptions.length; i++) {
-        optionsAsString += "<option value='" + UASoptions[i] + "'";
-        if(UASoptions[i]==settings[domain][0]){optionsAsString+="selected='selected'";}
-        optionsAsString += ">" + UASoptions[i] + "</option>";
-    }
-    newRow["User-Agent String"] = "<select name='UASdropdown'>"+optionsAsString+"</select>";
+    // var optionsAsString = "";
+    // for(var i = 0; i < UASoptions.length; i++) {
+    //     optionsAsString += "<option value='" + UASoptions[i] + "'";
+    //     if(UASoptions[i]==settings[domain][0]){optionsAsString+="selected='selected'";}
+    //     optionsAsString += ">" + UASoptions[i] + "</option>";
+    // }
+    // newRow["User-Agent String"] = "<select name='UASdropdown'>"+optionsAsString+"</select>";
     // Add probability column
     newRow["Probability"] = "<input name='probInput' class='probInput' type='number' value='"+settings[domain][1]+"' min='0' max='100'></input>%";
 
     tableData.push(newRow);
   }
 
-  var columns = ["","Domain","User-Agent String","Probability"];  //addAllColumnHeaders(tableData, selector);
+  var columns = ["","Active","Domain","Probability"];  //addAllColumnHeaders(tableData, selector);
   var headerTr$ = $('<tr/>');
   headerTr$.append($('<th/>').html(""));
+  headerTr$.append($('<th/>').html("Active"));
   headerTr$.append($('<th/>').html("Domain"));
-  headerTr$.append($('<th/>').html("User-Agent String"));
   headerTr$.append($('<th/>').html("Probability"));
   $(selector).append(headerTr$);
 
@@ -115,14 +170,16 @@ window.onload = initPage();
 
 //TODO: check if any entries in table before going through it..
 
+var testGlobal = null;
 
 function saveSettings(){
   unsaved = false;
 
   $( "#save" ).addClass( "onclic");
+  $( "#saveG" ).addClass( "onclic");
 
   var domainList_new = []
-  var settigns_new = {}
+  var settings_new = {}
   var oTable = document.getElementById('optionsTable');
 
   //gets rows of table
@@ -134,23 +191,38 @@ function saveSettings(){
   for (var i = 1; i < rowLength; i++){
 
     //gets cells of current row  
-    var newDomain = oTable.rows[i].cells[1].children[0].value;
+    var newActive = oTable.rows[i].cells[1].children[0].children[0].checked;
+    var newDomain = oTable.rows[i].cells[2].children[0].value;
     var newProb = oTable.rows[i].cells[3].children[0].value;
 
     //Validation of domain ( should be of form "google.com" - not contain any www. or https://, etc...)
     if (newDomain == "" || newProb == ""){
+      //highlight offending row
+      oTable.rows[i].classList.add("error-row");
       $( "#save" ).removeClass( "onclic" );
+      $( "#saveG" ).removeClass( "onclic" );
+
       alert("Did not save! Blank values are not allowed for domains or probabilities.");
       return;
     }
-
+    else{
+      oTable.rows[i].classList.remove("error-row");
+    }
+    // Update domain list
      domainList_new.push("*://*."+newDomain+"/*");
-
-     settigns_new[oTable.rows[i].cells[1].children[0].value] = [oTable.rows[i].cells[2].children[0].value, newProb];
-
-     
+    // Update settings list
+     settings_new[newDomain] = [newActive, newProb];
   }
-  chrome.storage.sync.set({settings: settigns_new}, function() {
+  //Get UAS Selections
+  var ddBrowser = document.getElementById("browserDropdown");
+  var ddOS = document.getElementById("osDropdown");
+  var saveNewUAS = {'os':ddOS.value, 'browser':ddBrowser.value};
+  document.getElementById('newUAS').textContent = UASlist[ddOS.value][ddBrowser.value]; 
+
+  chrome.storage.sync.set({setUAS: saveNewUAS}, function() {
+      console.log("new UAS updated");
+    });
+  chrome.storage.sync.set({settings: settings_new}, function() {
       console.log("Settings data updated");
     });
   chrome.storage.sync.set({domainList: domainList_new}, function() {
@@ -159,16 +231,21 @@ function saveSettings(){
 
   setTimeout(function() {
       $( "#save" ).removeClass( "onclic" );
+      $( "#saveG" ).removeClass( "onclic" );
       $( "#save" ).addClass( "validate");
+      $( "#saveG" ).addClass( "validate");
       document.getElementById("save").innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+      document.getElementById("saveG").innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
 
-    }, 1250 );
+    }, 1000 );
         
     setTimeout(function() {
       $( "#save" ).removeClass( "validate" );
+      $( "#saveG" ).removeClass( "validate" );
       document.getElementById("save").innerHTML = '<i class="fas fa-save"></i> Save';
+      document.getElementById("saveG").innerHTML = '<i class="fas fa-save"></i> Save';
 
-    }, 2250 );
+    }, 2000 );
 
 }
 
@@ -177,14 +254,14 @@ function saveSettings(){
  $("#insert-more").click(function () {
      $("#optionsTable").each(function () {
          var tds = '<tr class="new-row">';
-          tds += '<td><div class="delRow"><i class="fas fa-minus-circle"></i> <span class="delTag">&nbspDelete</span></div></td><td><input name="domainInput" class="domainInput" placeholder="google.com" type="text"></td><td><select name="UASdropdown"><option value="iOS">iOS</option><option value="Chrome">Chrome</option><option value="Safari">Safari</option><option value="Firefox">Firefox</option></select></td><td><input name="probInput" class="probInput" type="number" value="100" min="0" max="100"></input>%</td>'
+          tds += '<td><div class="delRow"><i class="fas fa-minus-circle"></i> <span class="delTag">&nbspDelete</span></div></td><td><label class="switch"><input type="checkbox"><span class="slider round"></span></label></td><td><input name="domainInput" class="domainInput" placeholder="example.com" type="text"></td><td><input name="probInput" class="probInput" type="number" value="100" min="0" max="100"></input>%</td>'
         tds += '</tr>';
         if ($('tbody', this).length > 0) {
             $('tbody', this).append(tds).children(':last').hide()
         .fadeIn(500);
         } else {
             console.log("no table entries");
-            $(this).append("<tr><th></th><th>Domain</th><th>User-Agent String</th><th>Probability</th></tr>");
+            $(this).append("<tr><th></th><th>Active</th><th>Domain</th><th>Probability</th></tr>");
             $(this).append(tds);
         }
     });
@@ -256,7 +333,7 @@ $("#optionsTable").on('click', '.delRow', function () {
         badRow.css("background-color","#FF3700");
         badRow.fadeOut(400, function(){
         badRow.remove();
-        saveSettings();
+        
             }); 
       }
 
@@ -266,6 +343,7 @@ $("#optionsTable").on('click', '.delRow', function () {
 
 //Save new options
 document.getElementById("save").onclick = saveSettings;
+document.getElementById("saveG").onclick = saveSettings;
 
 //alert user if unsaved changes exist
 function unloadPage(){ 
@@ -275,5 +353,4 @@ function unloadPage(){
 }
 
 window.onbeforeunload = unloadPage;
-
 
